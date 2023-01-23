30be5373c2036b6c47cda53eea1162e18610e02d
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-01-23 11:06:16 +0000

    Fix plugin name

--- Template/config/sidebar.php
+++ Template/config/sidebar.php
@@ -1,3 +1,3 @@
-<li <?= $this->app->checkMenuSelection('ApplicationMetadataController', 'show', 'ApplicationMeta') ?>>
+<li <?= $this->app->checkMenuSelection('ApplicationMetadataController', 'show', 'ApplicationMetadata') ?>>
     <?= $this->url->link('Site Metadata', 'ApplicationMetadataController', 'show', ['plugin' => 'ApplicationMetadata']) ?>
 </li>
