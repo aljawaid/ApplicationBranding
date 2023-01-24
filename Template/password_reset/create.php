98315e22d8f46a437859b3b644b0cfdff68a4d9d
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-01-24 17:30:56 +0000

    Add footer for Password Reset page

--- Template/password_reset/create.php
+++ Template/password_reset/create.php
@@ -23,3 +23,17 @@
         </div>
     </form>
 </div>
+<footer class="copyright">
+    <span class="center">&copy;&nbsp;
+        <?php if (!empty($this->task->configModel->get('app_rename'))): ?>
+            <?= $this->task->configModel->get('app_rename') ?>
+        <?php else: ?>
+            <?= t('My Workspace') ?>
+        <?php endif ?>
+        <?php if (!empty($this->task->configModel->get('copyright_from'))): ?>
+            <?= $this->task->configModel->get('copyright_from') ?>-<?= date("Y"); ?>
+        <?php else: ?>
+            <?= date("Y"); ?>
+        <?php endif ?>
+    </span>
+</footer>
