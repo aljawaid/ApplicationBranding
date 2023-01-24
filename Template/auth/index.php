9b8c6f24bb519f047a558b1f564eacba309ac547
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-01-24 09:41:49 +0000

    Add Placeholder

--- Template/auth/index.php
+++ Template/auth/index.php
@@ -60,7 +60,7 @@
                                 <g id="" stroke-linecap="round" stroke-linejoin="round"></g>
                                 <g id=""> <g id="" data-name="Layer 2"> <g id="" data-name="invisible box"> <rect width="48" height="48" fill="none"></rect> </g> <g id="" data-name="Layer 7"> <g> <path d="M39,18H35V13A11,11,0,0,0,24,2H22A11,11,0,0,0,11,13v5H7a2,2,0,0,0-2,2V44a2,2,0,0,0,2,2H39a2,2,0,0,0,2-2V20A2,2,0,0,0,39,18ZM15,13a7,7,0,0,1,7-7h2a7,7,0,0,1,7,7v5H15ZM37,42H9V22H37Z"></path> <circle cx="15" cy="32" r="3"></circle> <circle cx="23" cy="32" r="3"></circle> <circle cx="31" cy="32" r="3"></circle> </g> </g> </g> </g>
                             </svg>
-                            <?= $this->form->password('password', $values, $errors, array('required', 'autocomplete="current-password"'), 'password-input') ?>
+                            <?= $this->form->password('password', $values, $errors, array('required', 'autocomplete="current-password" placeholder="Required"'), 'password-input') ?>
                         </span>
 
                         <?php if (isset($captcha) && $captcha): ?>
