5f4fc77d45af2ef8a714cf6325c4014289aa55f0
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-01-25 01:05:52 +0000

    Add `meta` `description` for Password Reset Page

--- Template/layout.php
+++ Template/layout.php
@@ -71,8 +71,13 @@
             $outgoingAction = $this->app->getRouterAction();
         ?>
 
+        <?php if (($incomingController == 'PasswordResetController') && ($outgoingAction =='create')): ?>
+            <meta name="description" content="<?= t('Change your password for this platform.') ?>">
+            <meta property="og:description" content="<?= t('Change your password for this platform.') ?>" />
+        <?php else: ?>
             <meta name="description" content="<?= t('Use this platform to manage your productivity using tasks inside project boards to track files, comments and activities.') ?>">
             <meta property="og:description" content="<?= t('Use this platform to manage your productivity using tasks inside project boards to track files, comments and activities.') ?>" />
+        <?php endif ?>
 
         <?php if (isset($page_title)): ?>
             <?php if (!empty($this->task->configModel->get('app_rename'))): ?>
