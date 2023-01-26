5a3d0fe817faac119442e07439b01fab65507025
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-01-26 04:47:22 +0000

    Add app name to email

--- Template/password_reset/email.php
+++ Template/password_reset/email.php
@@ -5,6 +5,11 @@
 <hr>
 <footer class="copyright">
     <span class="center">&copy;&nbsp;
+        <?php if (!empty($this->task->configModel->get('app_rename'))): ?>
+            <?= $this->task->configModel->get('app_rename') ?>
+        <?php else: ?>
+            <?= t('My Workspace') ?>
+        <?php endif ?>
         <?php if (!empty($this->task->configModel->get('copyright_from'))): ?>
             <?= $this->task->configModel->get('copyright_from') ?>-<?= date("Y"); ?>
         <?php else: ?>
