68cb56b9dbc03d18898f614340a9a45589427f05
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-02-01 01:20:21 +0000

    FIX: Add Missing `alt` Attribute
    - #6

--- Template/password_reset/create.php
+++ Template/password_reset/create.php
@@ -6,7 +6,7 @@
     }
 </style>
 <section class="login-logo">
-    <img src="<?= $this->url->dir() ?>plugins/ApplicationBranding/Assets/img/workspace-icon-500x500.png" class="ws-logo">
+    <img src="<?= $this->url->dir() ?>plugins/ApplicationBranding/Assets/img/workspace-icon-500x500.png" class="ws-logo" alt="<?= t('Workspace logo') ?>">
     <h3 class="no-top no-bottom login-title">
         <?php if (!empty($this->task->configModel->get('app_rename'))): ?>
             <?= strtoupper($this->task->configModel->get('app_rename')) ?>
