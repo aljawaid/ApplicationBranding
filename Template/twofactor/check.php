e212078845120f8463cc947db018a895e21e654a
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-02-01 12:37:41 +0000

    New 2FA Page: Build Form: Better Form
    - WIP #6

--- Template/twofactor/check.php
+++ Template/twofactor/check.php
@@ -7,18 +7,19 @@
             <?= strtoupper(t('My Workspace')) ?>
         <?php endif ?>
     </h3>
-    <div class="page-header">
-        <h2><?= t('Two factor authentication') ?></h2>
-    </div>
-    <form method="post" action="<?= $this->url->href('TwoFactorController', 'check', array('user_id' => $this->user->getId())) ?>">
-        <?= $this->form->csrf() ?>
-
-        <?= $this->form->label(t('Code'), 'code') ?>
-        <?= $this->form->text('code', array(), array(), array('placeholder="123456"', 'autofocus', 'autocomplete="one-time-code"', 'pattern="[0-9]*"', 'inputmode="numeric"'), 'form-numeric') ?>
-
-        <div class="form-actions">
-            <button type="submit" class="btn btn-blue"><?= t('Check my code') ?></button>
-            <button type="button" class="btn btn-blue back-btn"><?= $this->url->link(t('Cancel'), 'AuthController', 'logout', array(), false, 'logout-button', t('Logout')) ?></button>
+    <div class="form-wrapper relative">
+        <div class="otp-area">
+            <div class="otp-form form-login relative">
+                <form method="post" action="<?= $this->url->href('TwoFactorController', 'check', array('user_id' => $this->user->getId())) ?>">
+                    <?= $this->form->csrf() ?>
+                    <?= $this->form->label(t('Authentication Code'), 'code') ?>
+                    <?= $this->form->text('code', array(), array(), array('placeholder="123456"', 'autofocus', 'autocomplete="one-time-code"', 'pattern="[0-9]*"', 'inputmode="numeric"'), 'form-numeric') ?>
+                    <div class="form-actions">
+                        <button type="submit" class="btn btn-blue"><?= t('Verify') ?></button>
+                        <button type="button" class="btn btn-blue back-btn"><?= $this->url->link(t('Cancel'), 'AuthController', 'logout', array(), false, 'logout-button', t('Logout')) ?></button>
+                    </div>
+                </form>
+            </div>
         </div>
-    </form>
+    </div>
 </section>
