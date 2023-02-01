6a7ef324c9d8a465fa23cd7f8e4daf43e31b7f3c
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-02-01 23:31:31 +0000

    New 2FA Page: Build Form: Better Placeholder
    - WIP #6

--- Template/twofactor/check.php
+++ Template/twofactor/check.php
@@ -24,8 +24,8 @@
                 <form method="post" action="<?= $this->url->href('TwoFactorController', 'check', array('user_id' => $this->user->getId())) ?>">
                     <?= $this->form->csrf() ?>
                     <?= $this->form->label(t('Authentication Code'), 'code') ?>
-                    <?= $this->form->text('code', array(), array(), array('placeholder="123456"', 'autofocus', 'autocomplete="one-time-code"', 'pattern="[0-9]*"', 'inputmode="numeric"'), 'form-numeric') ?>
                     <span class="required-wrapper-otp relative">
+                        <?= $this->form->text('code', array(), array(), array('placeholder="XXXXXX"', 'autofocus', 'autocomplete="one-time-code"', 'pattern="[0-9]*"', 'inputmode="numeric"'), 'form-numeric otp-field') ?>
                     </span>
                     <div class="form-actions">
                         <button type="submit" class="btn btn-blue"><?= t('Verify') ?></button>
