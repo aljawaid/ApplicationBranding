b3359325ed862ce7f52986d5dbebcec16c595bd7
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-02-02 19:47:43 +0000

    Better FIX: Move `Save Settings` Button
    - move to where settings actually are
    - #7

--- Template/config/branding-settings.php
+++ Template/config/branding-settings.php
@@ -34,10 +34,10 @@
                 <p class="form-help"><?= t('If no year is entered, then the standard copyright message with the current year is shown') ?></p>
             </div>
         </div>
+        <div class="form-actions">
+            <button type="submit" class="btn btn-blue" title="<?= t('Saves any changes made on this page') ?>"><?= t('Save Settings') ?></button>
+        </div>
     </fieldset>
-    <div class="form-actions">
-        <button type="submit" class="btn btn-blue" title="<?= t('Saves any changes made on this page') ?>"><?= t('Save Settings') ?></button>
-    </div>
     <fieldset class="site-metadata-settings">
         <legend class=""><?= t('Site Metadata') ?></legend>
         <p class=""><?= t('The site description and common device icons are shown in') ?> <?= $this->url->link(t('Site Metadata'), 'ApplicationBrandingController', 'show', ['plugin' => 'ApplicationBranding'], false, 'application-link', t('Visit page'), false, '') ?></p>
