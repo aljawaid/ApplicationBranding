b05191919f8daadbf3e444546caa11adf85d5b85
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-02-05 07:17:14 +0000

    Customizer Compatibility
    - Add site logo
    - WIP #4
    - Relates to https://github.com/creecros/Customizer/issues/141

--- Template/header/title_customizer.php
+++ Template/header/title_customizer.php
@@ -1,7 +1,15 @@
 <h1 class="header-title">
-    <span class="logo">
-        <?= $this->url->link('<img src="' . $this->url->dir() . 'plugins/ApplicationBranding/Assets/img/workspace-icon-500x500.png" width="40px" style="vertical-align: middle;"  alt="' . t('Workspace logo') .'">', 'DashboardController', 'show', array(), false, '', t('Dashboard')) ?>
-    </span>
+    <?php if (null !== $this->task->customizerFileModel->getByType(1)) : ?>
+        <span class="logo">
+            <a href="<?= $this->url->href('DashboardController', 'show', array(), false, '', t('Dashboard')) ?>" title="<?= t('Dashboard') ?>">
+                <img src="<?= $this->url->href('CustomizerFileController', 'image', array('plugin' => 'customizer', 'file_id' => $this->task->customizerFileModel->getIdByType(1))) ?>" height="<?= $this->task->configModel->get('headerlogo_size', '30') ?>">
+            </a>
+        </span>
+    <?php else: ?>
+        <span class="logo">
+            <?= $this->url->link('<img src="' . $this->url->dir() . 'plugins/ApplicationBranding/Assets/img/workspace-icon-500x500.png" width="40px" style="vertical-align: middle;"  alt="' . t('Workspace logo') .'">', 'DashboardController', 'show', array(), false, '', t('Dashboard')) ?>
+        </span>
+    <?php endif ?>
     <span class="title">
         <?php if (! empty($project) && ! empty($task)): ?>
             <?= $this->url->link($this->text->e($project['name']), 'BoardViewController', 'show', array('project_id' => $project['id'])) ?>
