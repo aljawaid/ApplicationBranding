aa17806151d8b42963c74f854035186ad2230617
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-08-17 03:21:28 +0100

    Migrate Icon `template-manager-icon-white`
    #15

--- Template/config/about.php
+++ Template/config/about.php
@@ -155,17 +155,7 @@
             <?php if (file_exists('plugins/TemplateManager')): ?>
                 <div class="box-wrapper back-deep-green">
                     <div class="box-icon">
-                        <svg width="28px" height="28px" class="template-manager-icon" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                            <g stroke-width="0"/>
-                            <g stroke-linecap="round" stroke-linejoin="round"/>
-                            <g>
-                                <path fill-rule="evenodd" clip-rule="evenodd" d="M3 3V9H21V3H3ZM19 5H5V7H19V5Z" fill="white"/>
-                                <path fill-rule="evenodd" clip-rule="evenodd" d="M3 11V21H11V11H3ZM9 13H5V19H9V13Z" fill="white"/>
-                                <path d="M21 11H13V13H21V11Z" fill="white"/>
-                                <path d="M13 15H21V17H13V15Z" fill="white"/>
-                                <path d="M21 19H13V21H21V19Z" fill="white"/>
-                            </g>
-                        </svg>
+                        <span class="template-manager-icon-white"></span>
                     </div>
                     <div class="box-title"><?= t('Templates') ?></div>
                     <div class="box-data"><span class="data-value bold"><?= $allTemplates ?></span></div>
