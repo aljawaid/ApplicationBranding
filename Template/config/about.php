3b04f865608eed5c741673c29a5b9f5a2b6bae8f
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-08-17 20:08:22 +0100

    Migrate Icon `description-icon-white`
    #15

--- Template/config/about.php
+++ Template/config/about.php
@@ -162,22 +162,7 @@
                 </div>
                 <div class="box-wrapper back-deep-green">
                     <div class="box-icon">
-                        <svg width="28px" height="28px" class="description-icon" fill="currentColor" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" enable-background="new 0 0 512 512" xml:space="preserve">
-                            <g stroke-width="0"/>
-                            <g stroke-linecap="round" stroke-linejoin="round"/>
-                            <g>
-                                <g>
-                                    <path d="M256,0C114.609,0,0,114.609,0,256s114.609,256,256,256s256-114.609,256-256S397.391,0,256,0z M256,472 c-119.297,0-216-96.703-216-216S136.703,40,256,40s216,96.703,216,216S375.297,472,256,472z"/>
-                                    <path fill="white" d="M320,128H160v256h192V164.578L320,128z M176,368V144h128v32h32v192H176z"/>
-                                    <rect fill="white" x="192" y="272" width="128" height="16"/>
-                                    <rect fill="white" x="192" y="240" width="128" height="16"/>
-                                    <rect fill="white" x="192" y="192" width="64" height="16"/>
-                                    <rect fill="white" x="192" y="160" width="32" height="16"/>
-                                    <rect fill="white" x="192" y="304" width="128" height="16"/>
-                                    <rect fill="white" x="192" y="336" width="128" height="16"/>
-                                </g>
-                            </g>
-                        </svg>
+                        <span class="description-icon-white"></span>
                     </div>
                     <div class="box-title"><?= t('Task Templates') ?></div>
                     <div class="box-data"><span class="data-value bold"><?= $allTaskTemplates ?></span></div>
