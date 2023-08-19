dda1e5dc12bee1708fd909bfe4e6275338ed868e
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-08-19 01:32:58 +0100

    Migrate Icon `manual-edits-icon`
    #15

--- Template/config/manual-edits.php
+++ Template/config/manual-edits.php
@@ -1,9 +1,6 @@
 <div id="ManualEditsPage" class="metadata-page-header">
     <h2 class="">
-        <svg width="32px" height="32px" class="manual-edits-icon" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-            <g stroke-width="0"></g><g stroke-linecap="round" stroke-linejoin="round"></g><g><path d="M3 23h18V6.709L15.29 1H3zM15 2h.2L20 6.8V7h-5zM4 2h10v6h6v14H4zm9.13 9.044l.938.35-3.199 8.583-.937-.35zm2.224 1.603l2.853 2.853-2.854 2.854-.707-.707 2.147-2.147-2.146-2.146zm-6 .707L7.207 15.5l2.146 2.146-.707.707L5.793 15.5l2.854-2.854z"></path><path fill="none" d="M0 0h24v24H0z"></path></g>
-        </svg>
-        <?= t('Manual Edits') ?>
+        <span class="manual-edits-icon"></span> <?= t('Manual Edits') ?>
     </h2>
     <p class="edits-page-desc"><?= t('To gain complete control over rebranding this application, some manual changes are required which are out of scope for Kanboard plugins. Following the simple changes below ensures your Kanboard installation is consistently named to your preferences.') ?></p>
     <section class="m-edit-section">
