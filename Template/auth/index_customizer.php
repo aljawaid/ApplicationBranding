1e0d2e5c073999138c5b002adfd3671bb1d15e32
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-08-19 20:48:13 +0100

    Migrate Icon `password-icon`
    #15

--- Template/auth/index_customizer.php
+++ Template/auth/index_customizer.php
@@ -52,11 +52,7 @@
                         </span>
                         <?= $this->form->label(t('Password'), 'password') ?>
                         <span class="required-wrapper relative">
-                            <svg class="password-icon" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" fill="#4D4D4D" stroke="0">
-                                <g id="" stroke-width="0"></g>
-                                <g id="" stroke-linecap="round" stroke-linejoin="round"></g>
-                                <g id=""> <g id="" data-name="Layer 2"> <g id="" data-name="invisible box"> <rect width="48" height="48" fill="none"></rect> </g> <g id="" data-name="Layer 7"> <g> <path d="M39,18H35V13A11,11,0,0,0,24,2H22A11,11,0,0,0,11,13v5H7a2,2,0,0,0-2,2V44a2,2,0,0,0,2,2H39a2,2,0,0,0,2-2V20A2,2,0,0,0,39,18ZM15,13a7,7,0,0,1,7-7h2a7,7,0,0,1,7,7v5H15ZM37,42H9V22H37Z"></path> <circle cx="15" cy="32" r="3"></circle> <circle cx="23" cy="32" r="3"></circle> <circle cx="31" cy="32" r="3"></circle> </g> </g> </g> </g>
-                            </svg>
+                            <span class="password-icon"></span>
                             <?= $this->form->password('password', $values, $errors, array('required', 'autocomplete="current-password" placeholder="Required"'), 'password-input') ?>
                         </span>
 
